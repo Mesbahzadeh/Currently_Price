07eda684b4f21161b8e1a62a305b3240eab14b65
Mehran Mesbahzadeh <mehran.mesbahzadeh@yahoo.com>
2025-12-16 11:17:00 +0330

    end

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,22 @@
-// vite.config.ts
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+export default defineConfig({
+  plugins: [react()],
+  base: '/Currently_Price/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: './index.html',
       },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      output: {
+        manualChunks: undefined,
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      },
-      // اضافه کردن base برای GitHub Pages
-      base: '/Currently_Price/',
-      build: {
-        outDir: 'dist',
-        sourcemap: false,
-        rollupOptions: {
-          output: {
-            manualChunks: undefined,
-          },
-        },
-      }
-    };
+    },
+  },
+  server: {
+    port: 3000,
+  }
 });
